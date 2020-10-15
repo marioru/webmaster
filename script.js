c6402d6880704e179ccc2691d211d5a6467e16ef
Marianne <14089618+marioru@users.noreply.github.com>
2020-10-15 14:05:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ window.onload = function() {
 		});
 	}
 }
+
+function getIP(json) {
+    IPaddress = json.ip;
+  
+  };
+
+  var $ = function(id) {
+    return document.getElementById(id);
+  };
+  var inc = 0;
+  var out = 0;
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
+  var t;
+  
+  var anim = function() {
+    inc++;
+    if (inc % 7 === 0 && out < IPaddress.length) {
+      $('anim').appendChild(document.createTextNode(IPaddress[out]));
+      out++;
+    } else if (out >= IPaddress.length) {
+      $('shuffle').innerHTML = '';
+      removeInterval(t);
+    }
+    
+    $('shuffle').innerHTML =
+      chars[Math.floor(Math.random() * chars.length)];
+  };
+  
+  t = setInterval(anim, 50);
+
+  
